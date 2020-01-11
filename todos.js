5faa3d8b3db202be1df38052bf0f7fbf47af1977
guidotti-dll <lucasguidotti16@gmail.com>
2020-01-11 17:47:41 -0200

    renderizando Todos

--- todos.js
+++ todos.js
@@ -1,9 +1,21 @@
-var listElements = document.querySelector('#app ul');
-var inputElements = document.querySelector('#app input');
-var buttonElements = document.querySelector('#app button');
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
 
 var todos = [
     'Fazer café',
     'Estudar Javascript',
     'Acessar comunidade da Rocketseat'
 ];
+
+function renderTodos() {
+    for (todo of todos) {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+    }
+};
+
+renderTodos();
